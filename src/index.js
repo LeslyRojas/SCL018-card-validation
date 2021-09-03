a4bcbc1db4c5de5c0d9ae0f8f10be236b0b31d95
Lesly Rojas <leslyrojast@gmail.com>
2021-09-03 13:29:25 -0400

    aplique selector de DOM y evento al boton, declaré variable que guarda  input+value L

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+const boton = document.getElementById("enviarNumero");
+
+boton.addEventListener("click", ()=>{
+    const numeroTarjeta = document.getElementById("numeroTarjeta").value;
+
+    console.log(numeroTarjeta)
+})
+
 import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
