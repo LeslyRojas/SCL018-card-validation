b244e7a9df0c320409efc7acba49c4e88ed5da79
Margarita Molina <molmaragrita@gmail.com>
2021-09-08 12:51:49 -0300

    Agregué condicional If else para multiplicary sacar sumaTotal M

--- src/validator.js
+++ src/validator.js
@@ -12,10 +12,19 @@ const validator = {
       const position = i + 2;
 
         if (position % 2 == 0){
-        const multiplicar = number * 2 }
- 
-    
-        }
-    }
-}
+        const multiplicar = number * 2;
+        if (multiplicar > 9) {
+        const separarNumeros = multiplicar.toString().split("");
+        const primerDigito = parseInt(separarNumeros[0]);
+        const segundoDigito = parseInt(separateNumbers[1]);
+        const sumarSeparados = primerDigito + segundoDigito;
+        sumaTotal += sumarSeparados;
+        } else{
+            sumaTotal += multiplicar;
+        }}}
+        console.log(sumaTotal);
+
 export default validator
+}
+
+}
