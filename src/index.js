8377f47c377a37a2c9528e1045e43d51bb0cb2ae
Margarita Molina <molmaragrita@gmail.com>
2021-09-12 19:41:10 -0300

    agregué función maskify,variable tarjetaOculta en index.js y ordené html M

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
 const boton = document.getElementById("enviarNumero");
 
-boton.addEventListener("click", ()=>{
-    const numeroTarjeta = document.getElementById("numeroTarjeta").value;
-    validator.isValid(numeroTarjeta);
-    
-if (validator.isValid(numeroTarjeta) === true) {
-        alert ("tarjeta válida");
-    } else {
-        alert ("tarjeta inválida");
-}
-    
-})
+boton.addEventListener("click", () => {
+  const numeroTarjeta = document.getElementById("numeroTarjeta").value;
+  validator.isValid(numeroTarjeta);
 
-
-
-
-
+  if (validator.isValid(numeroTarjeta) === true) {
+    alert("tarjeta válida");
+    //declara variable que llama el valor devuelto por la función maskify
+    let tarjetaOculta = validator.maskify(numeroTarjeta);
+    //poner el valor de la tarjeta oculta en el input
+    document.getElementById("numeroTarjeta").value = tarjetaOculta;
+  } else {
+    alert("tarjeta inválida");
+  }
+});
--- src/validator.js
+++ src/validator.js
@@ -1,42 +1,49 @@
-
 const validator = {
-    isValid (numeroTarjeta) {
-
+  isValid(numeroTarjeta) {
     let invertidos = numeroTarjeta.toString().split("").reverse();
-    
-     
+
     let sumaTotal = 0;
-    
+
     for (let i = 0; i < invertidos.length; i++) {
-      
-        if (i % 2 === 1){
-            const multiplicar = invertidos[i] * 2;  
-            if (multiplicar > 9) {
-                const separarNumeros = multiplicar.toString().split("");
-                const primerDigito = parseInt(separarNumeros[0]);
-                const segundoDigito = parseInt(separarNumeros[1]);
-                const sumarSeparados = primerDigito + segundoDigito;
-                
-                sumaTotal += sumarSeparados;
-            } else{
-                sumaTotal += multiplicar; console.log(multiplicar);
-            }
-        }    else{
-                sumaTotal += parseInt(invertidos[i]);
+      if (i % 2 === 1) {
+        const multiplicar = invertidos[i] * 2;
+        if (multiplicar > 9) {
+          const separarNumeros = multiplicar.toString().split("");
+          const primerDigito = parseInt(separarNumeros[0]);
+          const segundoDigito = parseInt(separarNumeros[1]);
+          const sumarSeparados = primerDigito + segundoDigito;
+
+          sumaTotal += sumarSeparados;
+        } else {
+          sumaTotal += multiplicar;
+          console.log(multiplicar);
         }
+      } else {
+        sumaTotal += parseInt(invertidos[i]);
+      }
     }
-            
 
-            if (sumaTotal % 10 === 0){
+    if (sumaTotal % 10 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  maskify(numeroTarjeta) {
+    //establece condición minima para aplicar enmascarado
+    if (numeroTarjeta.length < 4) return numeroTarjeta;
+    //declara variables para mediante slice, devolver una copia de los elementos especificados del array original
+    let digitosTarjeta = numeroTarjeta.slice("");
+    //el índice negativo especifica un desplazamiento desde el final del array y extrae los ultimos 4 elementos del array
+    let ultimosDigitos = digitosTarjeta.slice(-4);
+    //declara variable del tipo de mascara
+    const simboloMascara = "#";
+    //declara variable para guardar el enmascaramiento tomando los numeros previos a los 4 ultimos, se le suma la variable de los ultimos digitos que serán visibles.
+    let tarjetaEnmascarada =
+      simboloMascara.repeat(digitosTarjeta.length - 4) + ultimosDigitos;
 
-                return true;
-               
-              } 
-              else {
-                
-                return false}
-   
-
-}
-}
+    //finalizar la ejecución de la función, especifica un valor a ser devuelto a quien llama la función
+    return tarjetaEnmascarada;
+  },
+};
 export default validator;
