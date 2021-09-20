3606d59c5181901007a7d83d77e5461184fc50b8
Margarita Molina <molmaragrita@gmail.com>
2021-09-20 12:51:01 -0300

    funcionalidad al input nombreUsuario M y L

--- src/index.js
+++ src/index.js
@@ -4,18 +4,19 @@ const boton = document.getElementById("enviarNumero");
 //aplicar selector DOM para hacer click y rescatar el valor del input en la variable numeroTarjeta
 boton.addEventListener("click", () => {
   const numeroTarjeta = document.getElementById("numeroTarjeta").value;
+  const nombreUsuario = document.getElementById("nombreUsuario").value;
 
   //validator.isValid(numeroTarjeta);
 
   //condicional si resultado de validación es true, envia alerta de tarjeta válida.
   if (validator.isValid(numeroTarjeta) === true) {
-    alert("tarjeta válida");
+    alert(nombreUsuario + ", " + "tu número de tarjeta es válido");
     //Solo si es válida, se declara variable que llama el valor devuelto por la función maskify
     let tarjetaOculta = validator.maskify(numeroTarjeta);
     //poner el valor de la tarjeta oculta en el input
     document.getElementById("numeroTarjeta").value = tarjetaOculta;
     //si el resultado de la validación no es true, emitir alerta de tarjeta inválida.
   } else {
-    alert("tarjeta inválida");
+    alert(nombreUsuario + "," + " " + "tu número de tarjeta es inválido");
   }
 });
